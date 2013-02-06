ab0f91c610b33f8b8c6cfc35c15dab664939ed39
Thomas Pedersen <thomas@cozybit.com>
2013-02-05 19:36:25 -0800

    mesh: add mesh_mpm and insert new station
    The MPM needs some basic station storage functions to
    function, but the existing AP ones rely on complicated,
    pretty AP-specific data structures. Instead create our own
    struct meshd_data and steal the AP station functions to
    operate on this.
    
    Good enough for now, but note there aren't yet any STA
    teardown functions!
    
    Signed-hostap: Thomas Pedersen <thomas@cozybit.com>

--- wpa_supplicant/mesh.h
+++ wpa_supplicant/mesh.h
@@ -9,10 +9,20 @@
 #ifndef MESH_H
 #define MESH_H
 
+struct meshd_data {
+	int max_num_sta;
+	int num_sta; /* number of entries in sta_list */
+	struct sta_info *sta_list; /* STA info list head */
+#define STA_HASH_SIZE 256
+#define STA_HASH(sta) (sta[5])
+	struct sta_info *sta_hash[STA_HASH_SIZE];
+};
+
 /**
  * mesh_iface - per-interface mesh data
  */
 struct mesh_iface {
+	struct meshd_data *bss;
 	u8 *ies;
 	int ie_len;
 };
