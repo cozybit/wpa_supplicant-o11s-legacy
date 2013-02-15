b20f2abda5959c1bafeb024145b6e6683de57d26
Thomas Pedersen <thomas@cozybit.com>
2013-02-14 18:33:51 -0800

    mesh: send initial SAE commit
    Signed-hostap: Thomas Pedersen <thomas@cozybit.com>

--- wpa_supplicant/mesh_rsn.c
+++ wpa_supplicant/mesh_rsn.c
@@ -125,19 +125,158 @@ struct mesh_rsn *mesh_rsn_auth_init(struct wpa_supplicant *wpa_s,
 	return mesh_rsn;
 }
 
+static int index_within_array(const int *array, int idx)
+{
+	int i;
+	for (i = 0; i < idx; i++) {
+		if (array[i] == -1)
+			return 0;
+	}
+	return 1;
+}
+
+static int mesh_rsn_sae_group(struct wpa_supplicant *wpa_s,
+			      struct sae_data *sae)
+{
+	int *groups = wpa_s->conf->sae_groups;
+	int default_groups[] = { 19, 20, 21, 25, 26 };
+
+	if (!groups)
+		groups = default_groups;
+
+	/* Configuration may have changed, so validate current index */
+	if (!index_within_array(groups, wpa_s->mesh_rsn->sae_group_index))
+		return -1;
+
+	for (;;) {
+		int group = groups[wpa_s->mesh_rsn->sae_group_index];
+		if (group < 0)
+			break;
+		if (sae_set_group(sae, group) == 0) {
+			wpa_dbg(wpa_s, MSG_DEBUG, "SME: Selected SAE group %d",
+				sae->group);
+		       return 0;
+		}
+		wpa_s->mesh_rsn->sae_group_index++;
+	}
+
+	return -1;
+}
+
+struct wpabuf *
+mesh_rsn_build_sae_commit(struct wpa_supplicant *wpa_s,
+			  struct wpa_ssid *ssid, struct sta_info *sta)
+{
+	struct wpabuf *buf;
+	int len;
+
+	if (ssid->passphrase == NULL) {
+		wpa_msg(wpa_s, MSG_DEBUG, "SAE: No password available");
+		return NULL;
+	}
+
+	if (mesh_rsn_sae_group(wpa_s, sta->sae) < 0) {
+		wpa_msg(wpa_s, MSG_DEBUG, "SAE: Failed to select group");
+		return NULL;
+	}
+
+	if (sae_prepare_commit(wpa_s->own_addr, sta->addr,
+			       (u8 *) ssid->passphrase,
+			       os_strlen(ssid->passphrase), sta->sae) < 0) {
+		wpa_msg(wpa_s, MSG_DEBUG, "SAE: Could not pick PWE");
+		return NULL;
+	}
+
+	len = wpa_s->mesh_rsn->sae_token ?
+		wpabuf_len(wpa_s->mesh_rsn->sae_token) : 0;
+	buf = wpabuf_alloc(4 + SAE_COMMIT_MAX_LEN + len);
+	if (buf == NULL)
+		return NULL;
+
+	sae_write_commit(sta->sae, buf, wpa_s->mesh_rsn->sae_token);
+
+	return buf;
+}
+
+static void mesh_rsn_send_auth(struct wpa_supplicant *wpa_s,
+			       const u8 *dst, const u8 *src,
+			       u16 auth_transaction, u16 resp,
+			       struct wpabuf *data)
+{
+	struct ieee80211_mgmt *auth;
+	u8 *buf;
+	size_t len;
+
+	len = IEEE80211_HDRLEN + sizeof(auth->u.auth) + wpabuf_len(data);
+	buf = os_zalloc(len);
+	if (buf == NULL)
+		return;
+
+	auth = (struct ieee80211_mgmt *) buf;
+	auth->frame_control = IEEE80211_FC(WLAN_FC_TYPE_MGMT,
+					   WLAN_FC_STYPE_AUTH);
+	os_memcpy(auth->da, dst, ETH_ALEN);
+	os_memcpy(auth->sa, src, ETH_ALEN);
+	os_memcpy(auth->bssid, dst, ETH_ALEN);
+
+	auth->u.auth.auth_alg = host_to_le16(WLAN_AUTH_SAE);
+	auth->u.auth.auth_transaction = host_to_le16(auth_transaction);
+	auth->u.auth.status_code = host_to_le16(resp);
+
+	if (data)
+		os_memcpy(auth->u.auth.variable,
+			  wpabuf_head(data), wpabuf_len(data));
+
+	wpa_msg(wpa_s, MSG_DEBUG, "authentication frame: STA=" MACSTR
+		   " auth_transaction=%d resp=%d (IE len=%lu)",
+		   MAC2STR(dst), auth_transaction,
+		   resp, (unsigned long) wpabuf_len(data));
+	if (wpa_drv_send_mlme(wpa_s, buf, len, 0) < 0)
+		perror("send_auth_reply: send");
+
+	os_free(buf);
+}
+
+/* initiate new SAE authentication with sta */
 int mesh_rsn_auth_sae_sta(struct wpa_supplicant *wpa_s,
 			  struct sta_info *sta)
 {
+	u16 resp = WLAN_STATUS_SUCCESS;
+	struct wpa_ssid *ssid = wpa_s->current_ssid;
+	struct wpabuf *buf;
+	int len;
+
 	if (!sta->sae) {
 		sta->sae = os_zalloc(sizeof(*sta->sae));
 		if (sta->sae == NULL)
 			return -1;
+		sta->sae->state = SAE_NOTHING;
+		sta->sae->send_confirm = 0;
 	}
 
-	sta->sae->state = SAE_NOTHING;
-	sta->sae->send_confirm = 0;
+	buf = mesh_rsn_build_sae_commit(wpa_s, ssid, sta);
+	if (!buf)
+		return -1;
+
+	sta->sae->state = SAE_COMMITTED;
+
 	wpa_msg(wpa_s, MSG_DEBUG,
-		"AUTH: initializing authentication with SAE peer: "
+		"AUTH: started authentication with SAE peer: "
 		MACSTR, MAC2STR(sta->addr));
+
+	/* probably do this */
+	/*
+	wpa_supplicant_set_state(wpa_s, WPA_AUTHENTICATING);
+	wpa_supplicant_rsn_supp_set_config(wpa_s, wpa_s->current_ssid);
+	*/
+
+	mesh_rsn_send_auth(wpa_s, sta->addr, wpa_s->own_addr,
+			   SAE_COMMITTED, WLAN_STATUS_SUCCESS, buf);
+
+	/* maybe MPM does this
+	eloop_register_timeout(SME_AUTH_TIMEOUT, 0, sme_auth_timer, wpa_s,
+			       NULL);
+			       */
+	wpabuf_free(buf);
 	return 0;
 }
