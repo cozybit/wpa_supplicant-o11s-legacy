86af3e05516aaaf27ac3d12ddb07fec272416494
Thomas Pedersen <thomas@cozybit.com>
2013-02-19 13:23:43 -0800

    hostapd: allow mesh authentication frames
    The AP authentication code may be processing a mesh
    authentication frame, so don't require the BSSID matches.
    
    Signed-hostap: Thomas Pedersen <thomas@cozybit.com>

--- src/ap/ieee802_11.c
+++ src/ap/ieee802_11.c
@@ -1719,9 +1719,11 @@ void ieee802_11_mgmt(struct hostapd_data *hapd, const u8 *buf, size_t len,
 	      stype == WLAN_FC_STYPE_ACTION) &&
 #endif /* CONFIG_P2P */
 	    os_memcmp(mgmt->bssid, hapd->own_addr, ETH_ALEN) != 0) {
+#ifndef CONFIG_MESH
 		printf("MGMT: BSSID=" MACSTR " not our address\n",
 		       MAC2STR(mgmt->bssid));
 		return;
+#endif /* CONFIG_MESH */
 	}
 
 
