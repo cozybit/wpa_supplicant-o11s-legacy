77a9e07fc312a2a052684dfeefc5e07e3abe4b8b
Chun-Yeow Yeoh <yeohchunyeow@gmail.com>
2013-03-07 01:47:13 +0800

    mesh_rsn: delete the non-existing header file
    crypto/aes_siv.h is not existing.
    
    Signed-off-by: Chun-Yeow Yeoh <yeohchunyeow@gmail.com>

--- wpa_supplicant/mesh_rsn.c
+++ wpa_supplicant/mesh_rsn.c
@@ -7,7 +7,6 @@
 #include "crypto/sha256.h"
 #include "crypto/random.h"
 #include "crypto/aes.h"
-#include "crypto/aes_siv.h"
 
 static void auth_logger(void *ctx, const u8 *addr, logger_level level,
 			const char *txt)
