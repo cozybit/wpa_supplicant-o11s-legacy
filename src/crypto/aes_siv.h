dd098986c45366c861090c618d74c28ca7d54513
Chun-Yeow Yeoh <yeohchunyeow@gmail.com>
2013-03-12 18:57:39 +0800

    crypto: add the include guard for aes_siv header file
    As pinpointed by Thomas.
    
    Signed-off-by: Chun-Yeow Yeoh <yeohchunyeow@gmail.com>

--- src/crypto/aes_siv.h
+++ src/crypto/aes_siv.h
@@ -7,9 +7,14 @@
  * See README for more details.
  */
 
+#ifndef AES_SIV_H
+#define AES_SIV_H
+
 int aes_siv_encrypt(const u8 *key, const u8 *pw,
 		    size_t pwlen, size_t num_elem,
 		    const u8 *addr[], const size_t *len, u8 *out);
 int aes_siv_decrypt(const u8 *key, const u8 *iv_crypt, size_t iv_c_len,
 		    int num_elem, const u8 *addr[], const size_t *len,
 		    u8 *out);
+
+#endif /* AES_SIV_H */
