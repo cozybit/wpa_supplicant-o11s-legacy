f1e06b9ec84aaec24a1a2dd60b682d127b9f3ca9
Thomas Pedersen <thomas@noack.us>
2014-03-10 18:40:34 -0700

    Makefile: add mesh support to wpa_s build
    Signed-off-by: Javier Lopez <jlopex@gmail.com>
    Signed-hostap: Thomas Pedersen <thomas@noack.us>
    
    --
    v8: squashed all makefile changes together, reordered, added android.mk
        bits

--- wpa_supplicant/Android.mk
+++ wpa_supplicant/Android.mk
@@ -208,6 +208,16 @@ endif
 ifdef CONFIG_WNM
 L_CFLAGS += -DCONFIG_WNM
 OBJS += wnm_sta.c
+ifdef CONFIG_MESH
+NEED_80211_COMMON=y
+NEED_AES_SIV=y
+NEED_AES_OMAC1=y
+NEED_AES_CTR=y
+L_CFLAGS += -DCONFIG_MESH
+OBJS += mesh.c
+OBJS += mesh_mpm.c
+OBJS += mesh_rsn.c
+endif
 endif
 
 ifdef CONFIG_TDLS
@@ -1148,6 +1158,9 @@ ifdef NEED_AES_ENC
 ifdef CONFIG_INTERNAL_AES
 AESOBJS += src/crypto/aes-internal-enc.c
 endif
+endif
+ifdef NEED_AES_SIV
+AESOBJS += src/crypto/aes-siv.c
 endif
 ifdef NEED_AES
 OBJS += $(AESOBJS)
