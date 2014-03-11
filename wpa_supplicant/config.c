67c8e635906aad5628156333c9bd43964628b6cf
Thomas Pedersen <thomas@noack.us>
2014-03-10 19:27:39 -0700

    config: add mesh mode, user_mpm, and no_auto_peer options
    Modify network mode to support mode number 5 when CONFIG_MESH is
    enabled.
    
    Add user_mpm config parameter, when this is set to 1 (the default) the
    peer link management is done on userspace, otherwise the peer management
    will be done by the kernel.
    
    Also add no_auto_peer parameter, which controls wheter a station will
    automatically initiate peering to another mesh peer that comes into
    range.
    
    Signed-off-by: Javier Lopez <jlopex@gmail.com>
    Signed-off-by: Jason Mobarak <x@jason.mobarak.name>
    Signed-hostap: Thomas Pedersen <thomas@noack.us>

--- wpa_supplicant/config.c
+++ wpa_supplicant/config.c
@@ -1679,7 +1679,12 @@ static const struct parse_data ssid_fields[] = {
 	{ INTe(fragment_size) },
 	{ INTe(ocsp) },
 #endif /* IEEE8021X_EAPOL */
+#ifdef CONFIG_MESH
+	{ INT_RANGE(mode, 0, 5) },
+	{ INT_RANGE(no_auto_peer, 0, 1) },
+#else
 	{ INT_RANGE(mode, 0, 4) },
+#endif /* CONFIG_MESH */
 	{ INT_RANGE(proactive_key_caching, 0, 1) },
 	{ INT_RANGE(disabled, 0, 2) },
 	{ STR(id_str) },
@@ -3240,6 +3245,7 @@ struct wpa_config * wpa_config_alloc_empty(const char *ctrl_interface,
 		return NULL;
 	config->eapol_version = DEFAULT_EAPOL_VERSION;
 	config->ap_scan = DEFAULT_AP_SCAN;
+	config->user_mpm = DEFAULT_USER_MPM;
 	config->fast_reauth = DEFAULT_FAST_REAUTH;
 	config->p2p_go_intent = DEFAULT_P2P_GO_INTENT;
 	config->p2p_intra_bss = DEFAULT_P2P_INTRA_BSS;
@@ -3778,6 +3784,9 @@ static const struct global_parse_data global_fields[] = {
 	{ INT_RANGE(eapol_version, 1, 2), 0 },
 	{ INT(ap_scan), 0 },
 	{ FUNC(bgscan), 0 },
+#ifdef CONFIG_MESH
+	{ INT(user_mpm), 0 },
+#endif /* CONFIG_MESH */
 	{ INT(disable_scan_offload), 0 },
 	{ INT(fast_reauth), 0 },
 	{ STR(opensc_engine_path), 0 },
