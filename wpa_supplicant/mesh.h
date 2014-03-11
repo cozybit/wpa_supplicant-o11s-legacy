778bac40fba0be646732d19b9cc82357f69e0837
Jason Abele <jason.abele@gmail.com>
2014-03-10 20:19:18 -0700

    wpa_supplicant: implement mesh scanning
    When mesh is configured in, include the wildcard
    mesh id so that mesh networks are returned.  Tag any
    returned mesh networks with [MESH].
    
    Signed-off-by: Javier Lopez <jlopex@gmail.com>
    Signed-hostap: Jason Abele <jason.abele@gmail.com>

--- wpa_supplicant/mesh.h
+++ wpa_supplicant/mesh.h
@@ -31,6 +31,8 @@ int wpa_supplicant_join_mesh(struct wpa_supplicant *wpa_s,
 int wpa_supplicant_leave_mesh(struct wpa_supplicant *wpa_s);
 void wpa_mesh_notify_peer(struct wpa_supplicant *wpa_s, const u8 *addr,
 			  const u8 *ies, int ie_len);
+void wpa_supplicant_mesh_add_scan_ie(struct wpa_supplicant *wpa_s,
+				     struct wpabuf **extra_ie);
 void wpa_supplicant_mesh_iface_deinit(struct wpa_supplicant *wpa_s,
 				      struct hostapd_iface *ifmsh);
 #endif /* MESH_H */
