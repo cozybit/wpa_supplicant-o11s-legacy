cb6c0b57d3d116425ad32535148bede76fdd70e7
Thomas Pedersen <thomas@noack.us>
2014-03-10 18:39:05 -0700

    Android.mk: add mesh support to wpa_s build
    Signed-off-by: Javier Lopez <jlopex@gmail.com>
    Signed-off-by: Jason Mobarak <x@jason.mobarak.name>
    Signed-hostap: Thomas Pedersen <thomas@noack.us>
    
    ---
    v5: - Fix the Android.mk files to actually build on Android.  Remove hunk
        that uncommented a debug config flag.
    
        - Split out the Android build fixes from the addition of mesh.
    
    v6: Updated email adddresses, added signed-hostap
    
    v7: remove blank lines before Signed-hostap

--- hostapd/Android.mk
+++ hostapd/Android.mk
@@ -14,6 +14,12 @@ endif
 
 ifeq ($(WPA_BUILD_HOSTAPD),true)
 
+# yes to secure mesh, XXX: should be a feature
+ifeq ($(WPA_SUPPLICANT_VERSION),MESH)
+  CONFIG_MESH=y
+  CONFIG_SAE=y
+endif
+
 include $(LOCAL_PATH)/android.config
 
 # To ignore possible wrong network configurations
@@ -229,6 +235,10 @@ endif
 ifdef CONFIG_WNM
 L_CFLAGS += -DCONFIG_WNM
 OBJS += src/ap/wnm_ap.c
+
+ifdef CONFIG_MESH
+L_CFLAGS += -DCONFIG_MESH
+endif
 endif
 
 ifdef CONFIG_IEEE80211N
