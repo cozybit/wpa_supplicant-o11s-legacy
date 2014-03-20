92698ccbd8f407b34f8b9a9eff4f56ca2440a335
Javier Cardona <javier@cozybit.com>
2014-03-20 16:55:59 -0700

    Fixup: Add no_auto_peer configuration option

--- wpa_supplicant/mesh.c
+++ wpa_supplicant/mesh.c
@@ -213,13 +213,6 @@ void wpa_mesh_notify_peer(struct wpa_supplicant *wpa_s, const u8 *addr,
 			  const u8 *ies, int ie_len)
 {
 	struct ieee802_11_elems elems;
-	struct wpa_ssid *ssid = wpa_s->current_ssid;
-
-	if (ssid && ssid->no_auto_peer) {
-		wpa_msg(wpa_s, MSG_INFO, "ignored new peer notification for "
-			MACSTR " because of no_auto_peer", MAC2STR(addr));
-		return;
-	}
 
 	wpa_msg(wpa_s, MSG_INFO,
 		"new peer notification for " MACSTR, MAC2STR(addr));
