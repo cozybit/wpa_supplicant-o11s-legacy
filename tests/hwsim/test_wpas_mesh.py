ecb874796158de1da4d9df2802d3f68117bc1ed8
Javier Cardona <javier@cozybit.com>
2014-03-20 12:26:22 -0700

    hwsim_tests: new test for mesh no_auto_peer
    Add tests for the 'no_auto_peer' case, where only one peer in the mesh
    initiates the peering.
    
    Signed-hostap: Javier Cardona <javier@cozybit.com>
    
    ---
    v2: Squash with "New test for no_auto_peer for secure mesh" https://github.com/cozybit/wpa_s_mesh_android/commit/d515ea024865beb5dd67a304c153b2b02b879235
    
    v5: Add Signed-off-by and updated commit detail.
    v6: Added signed-hostap

--- tests/hwsim/test_wpas_mesh.py
+++ tests/hwsim/test_wpas_mesh.py
@@ -227,6 +227,40 @@ def test_wpas_mesh_open(dev, apdev):
     return wrap_wpas_mesh_test(_test_wpas_mesh_open, dev, apdev)
 
 
+def _test_wpas_mesh_open_no_auto(dev, apdev, test_connectivity):
+    """wpa_supplicant open MESH network connectivity"""
+    id = dev[0].add_network()
+    dev[0].set_network(id, "mode", "5")
+    dev[0].set_network_quoted(id, "ssid", "wpas-mesh-open")
+    dev[0].set_network(id, "key_mgmt", "NONE")
+    dev[0].set_network(id, "frequency", "2412")
+    dev[0].mesh_group_add(id)
+
+    id = dev[1].add_network()
+    dev[1].set_network(id, "mode", "5")
+    dev[1].set_network_quoted(id, "ssid", "wpas-mesh-open")
+    dev[1].set_network(id, "key_mgmt", "NONE")
+    dev[1].set_network(id, "frequency", "2412")
+    dev[1].set_network(id, "no_auto_peer", "1")
+    dev[1].mesh_group_add(id)
+
+    # Check for mesh joined
+    check_mesh_group_added(dev[0])
+    check_mesh_group_added(dev[1])
+
+    # Check for peer connected
+    check_mesh_peer_connected(dev[0])
+    check_mesh_peer_connected(dev[1])
+
+    # Test connectivity 0->1 and 1->0
+    test_connectivity(dev[0], dev[1])
+    test_connectivity(dev[1], dev[0])
+
+
+def test_wpas_mesh_open_no_auto(dev, apdev):
+    return wrap_wpas_mesh_test(_test_wpas_mesh_open_no_auto, dev, apdev)
+
+
 def _test_wpas_mesh_secure(dev, apdev, test_connectivity):
     """wpa_supplicant secure MESH network connectivity"""
     id = dev[0].add_network()
@@ -260,3 +294,39 @@ def _test_wpas_mesh_secure(dev, apdev, test_connectivity):
 
 def test_wpas_mesh_secure(dev, apdev):
     return wrap_wpas_mesh_test(_test_wpas_mesh_secure, dev, apdev)
+
+
+def _test_wpas_mesh_secure_no_auto(dev, apdev, test_connectivity):
+    """wpa_supplicant secure MESH network connectivity"""
+    id = dev[0].add_network()
+    dev[0].set_network(id, "mode", "5")
+    dev[0].set_network_quoted(id, "ssid", "wpas-mesh-sec")
+    dev[0].set_network(id, "key_mgmt", "SAE")
+    dev[0].set_network(id, "frequency", "2412")
+    dev[0].set_network_quoted(id, "psk", "thisismypassphrase!")
+    dev[0].mesh_group_add(id)
+
+    id = dev[1].add_network()
+    dev[1].set_network(id, "mode", "5")
+    dev[1].set_network_quoted(id, "ssid", "wpas-mesh-sec")
+    dev[1].set_network(id, "key_mgmt", "SAE")
+    dev[1].set_network(id, "frequency", "2412")
+    dev[1].set_network_quoted(id, "psk", "thisismypassphrase!")
+    dev[1].set_network(id, "no_auto_peer", "1")
+    dev[1].mesh_group_add(id)
+
+    # Check for mesh joined
+    check_mesh_group_added(dev[0])
+    check_mesh_group_added(dev[1])
+
+    # Check for peer connected
+    check_mesh_peer_connected(dev[0])
+    check_mesh_peer_connected(dev[1])
+
+    # Test connectivity 0->1 and 1->0
+    test_connectivity(dev[0], dev[1])
+    test_connectivity(dev[1], dev[0])
+
+
+def test_wpas_mesh_secure_no_auto(dev, apdev):
+    return wrap_wpas_mesh_test(_test_wpas_mesh_secure_no_auto, dev, apdev)
