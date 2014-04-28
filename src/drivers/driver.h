4a5994803d0f8b5b9940339e0a6b7620b626bab6
Jason Mobarak <x@jason.mobarak.name>
2014-04-28 14:26:12 -0700

    wpa_s: enable mesh HT mode
    Add a new option "mesh_ht_mode" that specifies the HT mode for the
    mesh, with this option on, mesh beacons, actions frames, and probe
    responses with include the appropriate HT information elements.
    
    Change the mesh tests to check for the presence of HT in the scan
    results, for this to work "disable_scan_offload" must be added to
    p2p*.conf.
    
    [original implementation by Chun-Yeow Yeoh <yeohchunyeow@gmail.com>]
    [some fixes by Masashi Honma <masashi.honma@gmail.com>]
    Signed-off-by: Ashok Nagarajan <ashok.dragon@gmail.com>
    Signed-off-by: Javier Cardona <javier@cozybit.com>
    Signed-off-by: Jason Mobarak <x@jason.mobarak.name>

--- src/drivers/driver.h
+++ src/drivers/driver.h
@@ -911,6 +911,7 @@ struct wpa_driver_mesh_join_params {
 	u8 *ies;
 	int ie_len;
 	int freq;
+	enum ht_mode ht_mode;
 	struct wpa_driver_mesh_bss_params conf;
 #define WPA_DRIVER_MESH_FLAG_USER_MPM	0x00000001
 #define WPA_DRIVER_MESH_FLAG_DRIVER_MPM	0x00000002
--- wpa_supplicant/config.c
+++ wpa_supplicant/config.c
@@ -1543,7 +1543,6 @@ static int wpa_config_parse_psk_list(const struct parse_data *data,
 	return 0;
 }
 
-
 #ifndef NO_CONFIG_WRITE
 static char * wpa_config_write_psk_list(const struct parse_data *data,
 					struct wpa_ssid *ssid)
@@ -1553,6 +1552,60 @@ static char * wpa_config_write_psk_list(const struct parse_data *data,
 #endif /* NO_CONFIG_WRITE */
 
 #endif /* CONFIG_P2P */
+
+#ifdef CONFIG_MESH
+static int wpa_config_parse_mesh_ht_mode(const struct parse_data *data,
+					 struct wpa_ssid *ssid, int line,
+					 const char *value)
+{
+	int htval = 0;
+
+	if (os_strcmp(value, "NOHT") == 0)
+		htval = CHAN_NO_HT;
+	else if (os_strcmp(value, "HT20") == 0)
+		htval = CHAN_HT20;
+	else if (os_strcmp(value, "HT40-") == 0)
+		htval = CHAN_HT40MINUS;
+	else if (os_strcmp(value, "HT40+") == 0)
+		htval = CHAN_HT40PLUS;
+	else {
+		wpa_printf(MSG_ERROR,
+			   "Line %d: no ht_mode configured.", line);
+		return -1;
+	}
+
+	wpa_printf(MSG_MSGDUMP, "mesh_ht_mode: 0x%x", htval);
+	ssid->mesh_ht_mode = htval;
+	return 0;
+}
+
+#ifndef NO_CONFIG_WRITE
+static char *wpa_config_write_mesh_ht_mode(const struct parse_data *data,
+					   struct wpa_ssid *ssid)
+{
+	char *val;
+	switch (ssid->mesh_ht_mode) {
+	default:
+		val = NULL;
+		break;
+	case CHAN_NO_HT:
+		val = "NOHT";
+		break;
+	case CHAN_HT20:
+		val = "HT20";
+		break;
+	case CHAN_HT40MINUS:
+		val = "HT40-";
+		break;
+	case CHAN_HT40PLUS:
+		val = "HT40+";
+		break;
+	}
+	return val ? os_strdup(val) : NULL;
+}
+#endif /* NO_CONFIG_WRITE */
+
+#endif /* CONFIG_MESH */
 
 /* Helper macros for network block parser */
 
@@ -1722,6 +1775,9 @@ static const struct parse_data ssid_fields[] = {
 	{ INT_RANGE(peerkey, 0, 1) },
 	{ INT_RANGE(mixed_cell, 0, 1) },
 	{ INT_RANGE(frequency, 0, 65000) },
+#ifdef CONFIG_MESH
+	{ FUNC(mesh_ht_mode) },
+#endif
 	{ INT(wpa_ptk_rekey) },
 	{ STR(bgscan) },
 	{ INT_RANGE(ignore_broadcast_ssid, 0, 2) },
@@ -2197,6 +2253,9 @@ void wpa_config_set_network_defaults(struct wpa_ssid *ssid)
 	ssid->eap.fragment_size = DEFAULT_FRAGMENT_SIZE;
 	ssid->eap.sim_num = DEFAULT_USER_SELECTED_SIM;
 #endif /* IEEE8021X_EAPOL */
+#ifdef CONFIG_MESH
+	ssid->mesh_ht_mode = DEFAULT_MESH_HT_MODE;
+#endif
 #ifdef CONFIG_HT_OVERRIDES
 	ssid->disable_ht = DEFAULT_DISABLE_HT;
 	ssid->disable_ht40 = DEFAULT_DISABLE_HT40;
